e76df2b9cea105887b70fda4efc6c47e2668cf36
unknown <katie.albee@gmail.com>
2018-04-21 11:09:36 -0500

    more javascripty fun

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 // Initialize Firebase
-  var config = {
+var config = {
     apiKey: "AIzaSyBKi68QDHEzj9NpLFtJ8c6EQ6gphPQV56M",
     authDomain: "employeedatabase-7f940.firebaseapp.com",
     databaseURL: "https://employeedatabase-7f940.firebaseio.com",
     projectId: "employeedatabase-7f940",
     storageBucket: "employeedatabase-7f940.appspot.com",
     messagingSenderId: "471795698024"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+var employeeName;
+var role;
+var startDate;
+var rate;
